f2e3459898d2a5e9d3779a4c1240c10c0aa77e3d
somer81 <sevinc.omer@gmail.com>
2021-02-03 23:48:53 +0300

    initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            
+
+            foreach (var p in productManager.GetAllByCategoryId(1))
+            {
+                Console.WriteLine(p.ProductName + " " + p.CategoryId);
+            }
+
+
+            foreach (var p in productManager.GetAllByUnitPrice(100, 1000))
+            {
+                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
+            }
+
+
+            foreach (var p in productManager.GetAll())
+            {
+                Console.WriteLine(p.ProductName);
+            }
+        }
+    }
+}
